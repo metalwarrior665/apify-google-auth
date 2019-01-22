2308e425782dedf8ffa3e0933ebd5daa83de65c4
metalwarrior665 <lukaskrivka@gmail.com>
2019-01-22 21:37:45 +0100

    updated html

--- submit-page.js
+++ submit-page.js
@@ -9,9 +9,9 @@ module.exports.authorize = (authorizeUrl) =>
     <title>Apify Google authorization</title>
   </head>
   <body>
-    <p style="${pCss} margin-top: 200px;"> Please open this URL, log in and allow Apify to handle the project. Copy the resulting code below. </p>
+    <p style="${pCss} margin-top: 200px;"> Please authorize by clicking the button bellow. Then copy/paste the resulting code into the field and submit. </p>
     <div style="text-align: center; margin-top: 50px">
-       <a target="_blank" style="${inputCss} text-decoration: none; margin: 0 auto; background-color: #00a6d0;" href="${authorizeUrl}"> AUTHORIZE URL </a>
+       <a target="_blank" style="${inputCss} text-decoration: none; margin: 0 auto; background-color: #00a6d0;" href="${authorizeUrl}"> AUTHORIZE </a>
     </div>
     <form method="POST" action=/authorize style="text-align: center; margin-top: 50px">
       <input placeholder= "your code" name="code" id="code" style="font-size: 14px;font-family: monospace,serif;color: #11181c; border-radius: 3px; border: 1px solid #ccc; padding: 10px 15px; width: 400px; height: 20px; margin: auto"/>
